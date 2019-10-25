4ba630163c5e85656ae4d6b0e83badecfded8ace
xlcod <alvaro.pernas@live.u-tad.com>
2019-10-25 14:26:59 +0200

    anado adapter

---
+++ app/src/main/java/com/utad/networking/DayAdapter.kt
@@ -0,0 +1,20 @@
+package com.utad.networking
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.utad.networking.data.Day
+import com.utad.networking.model.City
+
+class DayAdapter():  RecyclerView.Adapter<DayAdapter.ViewHolder>()  {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DayAdapter.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: DayAdapter.ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
