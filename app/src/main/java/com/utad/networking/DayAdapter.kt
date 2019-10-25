87ab47304a7f28595c47dc5e31b2a98e9b354c7b
dmg2112 <daviddeozz@hotmail.com>
2019-10-25 14:33:08 +0200

    creando dayAdapter

--- app/src/main/java/com/utad/networking/DayAdapter.kt
+++ app/src/main/java/com/utad/networking/DayAdapter.kt
@@ -1,5 +1,7 @@
 package com.utad.networking
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.utad.networking.data.Day
@@ -16,5 +18,19 @@ class DayAdapter():  RecyclerView.Adapter<DayAdapter.ViewHolder>()  {
 
     override fun onBindViewHolder(holder: DayAdapter.ViewHolder, position: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+    class ViewHolder private constructor(view: View): RecyclerView.ViewHolder(view) {
+        private val
+
+        fun bind(day: Day) {
+
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.city_item, parent, false)
+                return ViewHolder(view)
+            }
+        }
     }
 }
