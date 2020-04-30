08fa7d99c86f21439626d65ee1948f65a0e024aa
Ricardo Campos <ricardocamposgarcia@gmail.com>
2020-04-30 09:28:07 +0200

    updated libraries

--- app/src/main/java/com/utad/networking/data/remote/WeatherApi.kt
+++ app/src/main/java/com/utad/networking/data/remote/WeatherApi.kt
@@ -4,7 +4,7 @@ import com.utad.networking.model.City
 import com.utad.networking.model.WeatherResponse
 import retrofit2.Response
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.*
 
 interface WeatherApi {
@@ -22,7 +22,7 @@ object RetrofitFactory {
     fun getWeatherApi(): WeatherApi {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
             .build().create(WeatherApi::class.java)
     }
 }
--- app/src/test/java/com/utad/networking/ui/citysearch/CitySearchPresenterTest.kt
+++ app/src/test/java/com/utad/networking/ui/citysearch/CitySearchPresenterTest.kt
@@ -7,11 +7,9 @@ import com.utad.networking.data.remote.RemoteRepository
 import com.utad.networking.model.City
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
-import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.ArgumentMatchers.anyString
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 
@@ -22,8 +20,6 @@ class CitySearchPresenterTest {
     @Mock private lateinit var remoteRepository: RemoteRepository
     @Mock private lateinit var localRepository: LocalRepository
     @Mock private lateinit var view: CitySearchView
-
-
 
     @Before
     fun setUp() {
@@ -37,23 +33,90 @@ class CitySearchPresenterTest {
     }
 
     @Test
-    fun searchClickedShouldShowResultsIfExists() = runBlocking {
-        val city = City(123, "City")
-        val search = "searchTerm"
-        whenever(remoteRepository.searchCities(search)).thenReturn(listOf(city))
+    fun shouldCallEmptyWhenEmptyResults() = runBlocking {
+        //configuracion
+        val searchTerm = "a"
+        whenever(remoteRepository.searchCities(searchTerm)).thenReturn(emptyList())
 
-        presenter.searchClicked(search)
+        //ejecucion
+        presenter.searchClicked(searchTerm)
 
-        verify(view).showCities(listOf(city))
-
+        //comprobacion
+        verify(view).showEmpty()
     }
 
-    @Test(expected = Exception::class)
-    fun shouldShowErrorWhenSearchFails() = runBlocking {
-        whenever(remoteRepository.searchCities("")).thenThrow()
+    @Test
+    fun shouldShowCitiesWhenResultsNotEmpty() = runBlocking {
+        val searchTerm = "a"
+        val citiesList = listOf(City(1, "city"))
+        whenever(remoteRepository.searchCities(searchTerm)).thenReturn(citiesList)
 
-        presenter.searchClicked("")
+        presenter.searchClicked(searchTerm)
 
-        verify(view).showError()
+        verify(view).showCities(citiesList)
     }
+
+    @Test
+    fun shouldShowLoadingWhileIsLoading() = runBlocking {
+        val searchTerm = "a"
+        whenever(remoteRepository.searchCities(searchTerm)).thenReturn(emptyList())
+
+        presenter.searchClicked(searchTerm)
+
+        verify(view).showEmpty()
+    }
+
+
+    //
+//    private lateinit var presenter: CitySearchPresenter
+//    @Mock private lateinit var remoteRepository: RemoteRepository
+//    @Mock private lateinit var localRepository: LocalRepository
+//    @Mock private lateinit var view: CitySearchView
+//
+//
+//    @Before
+//    fun setUp() {
+//        presenter = CitySearchPresenter(
+//            view,
+//            remoteRepository,
+//            localRepository,
+//            Dispatchers.Unconfined,
+//            Dispatchers.Unconfined
+//        )
+//    }
+//
+//    @Test
+//    fun searchClickedShouldShowResultsIfExists() = runBlocking {
+//        val city = City(123, "City")
+//        val search = "searchTerm"
+//        whenever(remoteRepository.searchCities(search)).thenReturn(listOf(city))
+//
+//        presenter.searchClicked(search)
+//
+//        verify(view).showCities(listOf(city))
+//
+//    }
+//
+//    @Test(expected = Exception::class)
+//    fun shouldShowErrorWhenSearchFails() = runBlocking {
+//        whenever(remoteRepository.searchCities("")).thenThrow()
+//
+//        presenter.searchClicked("")
+//
+//        verify(view).showError()
+//    }
+//
+//    @Test
+//    fun shouldShowLoadingWhileDataIsLoading() = runBlocking {
+//        val city = City(123, "City")
+//        val search = "searchTerm"
+//        whenever(remoteRepository.searchCities(search)).thenReturn(listOf(city))
+//
+//        presenter.searchClicked(search)
+//
+//        verify(view).showLoading()
+//        verify(view).showCities(listOf(city))
+//        verify(view).hideLoading()
+//
+//    }
 }
